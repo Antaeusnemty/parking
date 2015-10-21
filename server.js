0f5f0824da39c5bd45f4976e201b6a35e73779c3
Antaeusnemty <gamingemail007@gmail.com>
2015-10-20 21:05:52 -0400

    added some server stuff trying to get the geo location to submit to a database and do stuff with it wit da map

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express"); 
+var server = express();
+
+
+//express.createServer(function(req, res) {
+
+//    var url = url.
+// });
+
+//server.use (express.static(__dirname/+"index.html"));
+
+server.get(demo)
