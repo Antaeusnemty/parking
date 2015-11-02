03e4d7fa17f5e4f7426d430775764b9b710dafd1
Antaeusnemty <gamingemail007@gmail.com>
2015-11-02 18:29:44 -0500

    merged mapking and parking used serverjs mapjs and indexhtml

--- js/map.js
+++ js/map.js
@@ -1,22 +1,81 @@
-var mapOptions = {
-    center: new google.maps.LatLng(29.650793, -82.326780),
-    zoom: 14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
-};
-var map = new google.maps.Map (document.getElementById("map"), mapOptions);
 
-google.maps.event.addDomListener(window, 'load', initialize);
+angular.module('myApp', ['uiGmapgoogle-maps'])
+    .config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
 
-var marker=new google.maps.Marker({
-  position:myCenter,
-  });
 
-var mapProp = {
-  center:myCenter,
-  zoom:5,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
- //   new google.maps.Map(HTMLElement,MAPOPTIONS)
- //    mapvariable.fitBounds();
 
- 
+
+    .controller("myCtrl", function($scope, $http){
+        $scope.map = {center: {latitude: 29.650793, longitude: -82.326780 }, zoom: 13 };
+        $scope.marker = {
+          id: 0,
+          coords: {
+            latitude: 0,
+            longitude: 0,
+          },
+          options: { draggable: false },
+          events: {
+              dragend: function (marker, eventName, args) {
+                $log.log('marker dragend');
+                var lat = marker.getPosition().lat();
+                var lon = marker.getPosition().lng();
+                $log.log(lat);
+                $log.log(lon);
+
+          $scope.marker.options = {
+            draggable: true,
+            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+            labelAnchor: "100 0",
+            labelClass: "marker-labels"
+                };
+              }
+            }
+          };
+
+        $scope.getLocation = function(){
+          if (navigator){
+            navigator.geolocation.getCurrentPosition(showPosition);
+          } else {
+            x.innerHTML="Geolocation is not supported by this browser."
+          }
+          console.log("Im looking i promise")
+
+    };
+
+    function showPosition(position){
+      $scope.marker.coords.latitude = position.coords.latitude;
+      $scope.marker.coords.longitude = position.coords.longitude;
+    }
+
+    $scope.saveSpot = function(){
+      console.log("before the spot");
+       var spot = {
+         longitude: $scope.marker.coords.longitude,
+         latitude: $scope.marker.coords.latitude,
+         available: true,
+
+       };
+       console.log("before the post");
+       $http.post("/api/spots", spot)
+            .then(function(response){
+              console.log(response.data);
+            });
+
+    };
+      $scope.removeSpot = function(lat,long){
+        var url = '/api/spots/'+lat.toFixed(1)+'/'+long.toFixed(1);
+        $http.put(url, {available: false})
+             .then(function(response){
+               $scope.marker.coords.latitude = 0;
+               $scope.marker.coords.longitude = 0;
+             });
+
+    };
+
+});
--- server.js
+++ server.js
@@ -1,12 +1,82 @@
 var express = require("express");
 var server = express();
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var Schema = mongoose.Schema;
+var spotSchema = new Schema({
+    latitude: Number,
+    longitude: Number,
+    available: Boolean,
+});
+
+mongoose.connect("mongodb://localhost/spotDB");
+
+var Spot = mongoose.model('Spot', spotSchema);
 
 server.use(express.static(__dirname+""));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get("/", function(req, res){
   res.sendFile("index.html");
 
 });
+
+server.post("/api/spots", function(req, res){
+  console.log(req.body);
+ var spot = new Spot({
+   latitude: req.body.latitude.toFixed(1),
+   longitude: req.body.longitude.toFixed(1),
+   available: req.body.available,
+ });
+ spot.save(function(err){
+   if(err) console.error(err);
+
+   console.log(spot);
+   res.json(spot);
+ });
+});
+
+server.get("/api/spots", function(req,res){
+  Spot.find({}, function(err, spots){
+    if(err) res.json({error: err});
+
+    res.json(spots);
+  });
+});
+
+server.get('/api/spots/:id', function (req, res){
+    Spot.find({_id:req.params.id}, function (err, spot){
+          if(err) res.json({error: err});
+
+          res.json(spot);
+  });
+});
+server.get('/api/spots/:lat/:lon', function (req, res) {
+    Spot.find({latitude:req.params.lat.toFixed(1), longitude:req.params.lon.toFixed(1)}, function(err, spot){
+          if(err) res.json({error: err});
+
+          res.json(spot);
+    });
+})
+
+server.put('/api/spots/:id', function (req, res){
+  Spot.findOneAndUpdate({_id: req.params.id}, {available: req.body.available} ,function(err, spot){
+    if(err) throw err;
+
+    res.json({message: "spot availability updated"});
+  });
+});
+
+server.put('/api/spots/:lat/:lon', function(req, res){
+    Spot.findOneAndUpdate({latitude: req.params.lat, longitude: req.params.lon}, {available: req.body.available} ,function(err, spot){
+        if(err) throw err;
+
+        res.json({message: 'updated!'});
+    });
+});
+
 server.listen(3030, function(){
     console.log("Now listening on port 3030");
 });
